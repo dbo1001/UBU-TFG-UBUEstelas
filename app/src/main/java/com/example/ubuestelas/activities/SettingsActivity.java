64be98ea5d8d138a9365a3c6d61a2d9ae069c2d7
Marcos <marcospena97@gmail.com>
2020-03-03 21:18:16 +0100

    WIP Pantalla de ajustes

---
+++ app/src/main/java/com/example/ubuestelas/activities/SettingsActivity.java
@@ -0,0 +1,53 @@
+package com.example.ubuestelas.activities;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.ubuestelas.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.settings, new SettingsFragment())
+                .commit();
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+
+    public void changeUpButton(){
+        if (getParent().equals(NavigationDrawerActivity.class)){
+
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+//                NavUtils.navigateUpFromSameTask(this);
+//                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
