662217d1fa59c84371689bb9567738fac44c572a
Marcos <marcospena97@gmail.com>
2020-03-10 19:08:43 +0100

    Solicitud de permiso de ubicación movida al inicio de la aplicación

--- app/src/main/java/com/example/ubuestelas/activities/MainActivity.java
+++ app/src/main/java/com/example/ubuestelas/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ubuestelas.activities;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,10 +15,14 @@ import com.example.ubuestelas.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
     }
 
     /*
