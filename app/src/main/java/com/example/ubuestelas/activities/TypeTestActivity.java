98a1b804141977e1cb4513856fab2b9dabd5b4a1
Marcos <marcospena97@gmail.com>
2020-03-25 15:54:21 +0100

    Modificación en prueba tipo test para test solo con texto

--- app/src/main/java/com/example/ubuestelas/activities/TypeTestActivity.java
+++ app/src/main/java/com/example/ubuestelas/activities/TypeTestActivity.java
@@ -7,6 +7,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
@@ -50,33 +51,43 @@ public class TypeTestActivity extends AppCompatActivity {
             fileToRead = new JSONObject(Util.loadJSONFromAsset(getApplicationContext(), fileNameMark));
             JSONArray options = fileToRead.getJSONArray("options");
             int optionsNumber = fileToRead.getInt("optionsNumber");
+            String type = fileToRead.getString("type");
             String question = fileToRead.getString("question");
             TextView textView = findViewById(R.id.question_text);
             textView.setText(question);
-            setOptions(options,optionsNumber);
+            setOptions(options,optionsNumber, type);
 
         }catch (JSONException e){
             e.printStackTrace();
         }
     }
 
-    public void setOptions(JSONArray options, int optionsNumber){
+    public void setOptions(JSONArray options, int optionsNumber, String type){
         RadioGroup rg = findViewById(R.id.options_group);
 
         for(int i =0; i<optionsNumber; i++){
             try {
                 RadioButton radioButton = new RadioButton(this);
                 JSONObject option = options.getJSONObject(i);
-                int resourceId = this.getResources().getIdentifier(option.getString("image"), "drawable", this.getPackageName());
-                radioButton.setCompoundDrawablesWithIntrinsicBounds(resourceId,0,0,0);
-                radioButton.setText(option.getString("image"));
-                radioButton.setTextColor(Color.WHITE);
+                if(type.equals("images")) {
+                    int resourceId = this.getResources().getIdentifier(option.getString("option"), "drawable", this.getPackageName());
+                    radioButton.setCompoundDrawablesWithIntrinsicBounds(resourceId, 0, 0, 0);
+                    radioButton.setText(option.getString("option"));
+                    radioButton.setTextColor(Color.TRANSPARENT);
 //                radioButton.setText(option.getString("image"));
 //                radioButton.setButtonDrawable(getResources().getDrawable(resourceId));
 //                radioButton.setButtonDrawable(resourceId);
-                radioButton.setId(i);//set radiobutton id and store it somewhere
-                RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
-                rg.addView(radioButton, params);
+//                    radioButton.setId(i);//set radiobutton id and store it somewhere
+                    RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+                    rg.addView(radioButton, params);
+                }else{
+                    radioButton.setText(option.getString("option"));
+                    radioButton.setTextColor(Color.BLACK);
+                    radioButton.setTextSize(24);
+//                    radioButton.setTypeface(getResources().getFont(R.font.romanica));
+                    RadioGroup.LayoutParams params = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+                    rg.addView(radioButton, params);
+                }
             }catch (JSONException e){
                 e.printStackTrace();
             }
@@ -97,69 +108,73 @@ public class TypeTestActivity extends AppCompatActivity {
 
     public void getOption(View view){
         RadioGroup rg = findViewById(R.id.options_group);
-        int selectedId = rg.getCheckedRadioButtonId();
-        RadioButton radioButton = (RadioButton) findViewById(selectedId);
-        attempts++;
-        try {
-            String correctAnswer = fileToRead.getString("correct");
-            double score;
-            if(correctAnswer.equals(radioButton.getText())){
-                if (attempts==1){
-                    score=100.00;
-                }else {
-                    score = Util.testScoreIfFail(this, fileToRead.getInt("optionsNumber"), attempts);
-                }
-                Toast.makeText(this,getString(R.string.correct) + ". " + getString(R.string.points_obtained, score), Toast.LENGTH_SHORT).show();
-                JSONObject obj;
-                try {
-                    obj = new JSONObject(Util.loadJSONFromFilesDir(this, "userInfo"));
-                    JSONArray marks = obj.getJSONArray("marks");
-                    for (int i = 0; i < marks.length(); i++) {
-                        JSONObject mark = marks.getJSONObject(i);
-                        if(markName.equals(mark.getString("mark"))){
-                            mark.put("solved", true);
+        if (rg.getCheckedRadioButtonId() != -1) {
+            int selectedId = rg.getCheckedRadioButtonId();
+            RadioButton radioButton = (RadioButton) findViewById(selectedId);
+            attempts++;
+            try {
+                String correctAnswer = fileToRead.getString("correct");
+                double score;
+                if (correctAnswer.equals(radioButton.getText())) {
+                    if (attempts == 1) {
+                        score = 100.00;
+                    } else {
+                        score = Util.testScoreIfFail(this, fileToRead.getInt("optionsNumber"), attempts);
+                    }
+                    Toast.makeText(this, getString(R.string.correct) + ". " + getString(R.string.points_obtained, score), Toast.LENGTH_SHORT).show();
+                    JSONObject obj;
+                    try {
+                        obj = new JSONObject(Util.loadJSONFromFilesDir(this, "userInfo"));
+                        JSONArray marks = obj.getJSONArray("marks");
+                        for (int i = 0; i < marks.length(); i++) {
+                            JSONObject mark = marks.getJSONObject(i);
                             if (markName.equals(mark.getString("mark"))) {
                                 mark.put("solved", true);
-                                if (score == 100) {
-                                    mark.put("color", "green");
-                                } else if (score == 0) {
-                                    mark.put("color", "red");
-                                } else if (score > 0 && score < 100) {
-                                    mark.put("color", "yellow");
-                                } else {
-                                    mark.put("color", "azure");
+                                if (markName.equals(mark.getString("mark"))) {
+                                    mark.put("solved", true);
+                                    if (score == 100) {
+                                        mark.put("color", "green");
+                                    } else if (score == 0) {
+                                        mark.put("color", "red");
+                                    } else if (score > 0 && score < 100) {
+                                        mark.put("color", "yellow");
+                                    } else {
+                                        mark.put("color", "azure");
+                                    }
                                 }
                             }
                         }
-                    }
-                    obj.put("marks", marks);
-                    double scoreFile = obj.getDouble("score");
-                    scoreFile += score;
-                    double scoreToFile = Math.round(scoreFile*100)/100.0;
+                        obj.put("marks", marks);
+                        double scoreFile = obj.getDouble("score");
+                        scoreFile += score;
+                        double scoreToFile = Math.round(scoreFile * 100) / 100.0;
 //                    String scoreFormat = String.format("%.2f",scoreFile);
 //                    Double scoreToFile = Double.valueOf(scoreFormat);
 //                    double scoreToFile = Double.parseDouble(scoreFormat);
-                    obj.put("score", scoreToFile);
-                    Util.writeJSONToFilesDir(this,"userInfo", obj.toString());
-                }catch (JSONException e){
-                    e.printStackTrace();
+                        obj.put("score", scoreToFile);
+                        Util.writeJSONToFilesDir(this, "userInfo", obj.toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    SharedPreferences scoreSP = getSharedPreferences("scoreEvent", 0);
+                    SharedPreferences.Editor scoreEditor = scoreSP.edit();
+                    scoreEditor.putString("score", String.valueOf(score));
+                    scoreEditor.commit();
+                    SharedPreferences nameFileSP = getSharedPreferences("nameFileSP", 0);
+                    SharedPreferences.Editor nameFileEditor = nameFileSP.edit();
+                    nameFileEditor.putString("fileName", markName);
+                    nameFileEditor.commit();
+                    Intent intent = new Intent(this, DidYouKnowActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(this, getString(R.string.incorrect) + ". " + getString(R.string.try_again), Toast.LENGTH_SHORT).show();
                 }
-                SharedPreferences scoreSP = getSharedPreferences("scoreEvent", 0);
-                SharedPreferences.Editor scoreEditor = scoreSP.edit();
-                scoreEditor.putString("score", String.valueOf(score));
-                scoreEditor.commit();
-                SharedPreferences nameFileSP = getSharedPreferences("nameFileSP", 0);
-                SharedPreferences.Editor nameFileEditor = nameFileSP.edit();
-                nameFileEditor.putString("fileName", markName);
-                nameFileEditor.commit();
-                Intent intent = new Intent(this, DidYouKnowActivity.class);
-                startActivity(intent);
-                finish();
-            }else{
-                Toast.makeText(this,getString(R.string.incorrect) + ". " + getString(R.string.try_again), Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        }catch (JSONException e){
-            e.printStackTrace();
+        }else{
+            Toast.makeText(this, getString(R.string.select_one) + ".", Toast.LENGTH_SHORT).show();
         }
 
     }
