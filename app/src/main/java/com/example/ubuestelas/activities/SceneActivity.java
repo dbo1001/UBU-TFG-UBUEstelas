d69682ceef9ac81562b507984875b9b00c33cb25
Marcos <mpc0082@alu.ubu.es>
2020-06-03 20:58:40 +0200

    Añadidos audio, gifs e información en fichero de guardado

--- app/src/main/java/com/example/ubuestelas/activities/SceneActivity.java
+++ app/src/main/java/com/example/ubuestelas/activities/SceneActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.example.ubuestelas.R;
 import com.example.ubuestelas.util.Util;
@@ -21,6 +26,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import pl.droidsonroids.gif.GifDrawable;
+import pl.droidsonroids.gif.GifImageView;
 
 /**
  * Actividad donde se muestra la bienvenida a la aplicación.
@@ -35,6 +43,10 @@ public class SceneActivity extends AppCompatActivity {
 
     private int counter =0;
 
+    MediaPlayer voice;
+
+    GifDrawable gifDrawable;
+
     /**
      * Inicializa la actividad con su respectivo layout.
      * Se carga el primer texto con el nombre que el usuario ha introducido.
@@ -45,6 +57,7 @@ public class SceneActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_escena);
         createJSONprogress();
+        clearSharedPreferences();
         SharedPreferences sharedPref= getSharedPreferences("nameActivity",0);
         String name = sharedPref.getString("name", "amigo");
         JSONObject obj;
@@ -58,6 +71,34 @@ public class SceneActivity extends AppCompatActivity {
             TextView textView = findViewById(R.id.scene_text);
             textView.setText(text);
 
+            int resourceGifId = this.getResources().getIdentifier(scene.getString("image"), "drawable", this.getPackageName());
+            GifImageView gifImageView = (GifImageView) findViewById(R.id.scene_gif);
+            gifImageView.setImageResource(resourceGifId);
+            gifDrawable =(GifDrawable) gifImageView.getDrawable();
+//            gifDrawable.setLoopCount(2);
+
+            int resourceAudioID = this.getResources().getIdentifier(scene.getString("audio"), "raw", this.getPackageName());
+            voice = MediaPlayer.create(this, resourceAudioID);
+            voice.start();
+            voice.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    gifDrawable.stop();
+                }
+            });
+//            VideoView videoView = (VideoView) findViewById(R.id.presentation_video);
+//            String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.waving_roman;
+//            Uri uri = Uri.parse(videoPath);
+//            videoView.setVideoURI(uri);
+//            videoView.start();
+
+//            WebView webView =  (WebView) findViewById(R.id.web_view_scene);
+//            webView.setBackgroundColor(Color.TRANSPARENT); //for gif without background
+//            String videoPath = "android.resource://" + getPackageName() + "/" + R.drawable.larito_waving;
+//            webView.loadUrl(videoPath);
+
+
+
 
         }catch (Exception e){
             e.printStackTrace();
@@ -69,25 +110,41 @@ public class SceneActivity extends AppCompatActivity {
      * @param view La vista que se ha clickado.
      */
     public void changeScene(View view){
-        ImageView imageView = findViewById(R.id.imagen_escena);
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.scene_gif);
+//        ImageView imageView = findViewById(R.id.imagen_escena);
         TextView textView = findViewById(R.id.scene_text);
         counter++;
         JSONObject obj;
+        if(voice.isPlaying()){
+            voice.stop();
+        }
 
         try{
             obj = new JSONObject(Util.loadJSONFromAsset(getApplicationContext(),"welcomeScenes.json"));
             JSONArray scenes = obj.getJSONArray("scenes");
             int scenesNumber = obj.getInt("scenesNumber");
-            if (counter >= scenesNumber){
+            if (counter == scenesNumber){
 //                Intent intent = new Intent(this, MapsActivity.class);
                 Intent intent = new Intent(this, NavigationDrawerActivity.class);
                 startActivity(intent);
                 finish();
             }else {
                 JSONObject scene = scenes.getJSONObject(counter);
-                final int resourceId = this.getResources().getIdentifier(scene.getString("image"), "drawable", this.getPackageName());
-                imageView.setImageResource(resourceId);
+                int resourceGifId = this.getResources().getIdentifier(scene.getString("image"), "drawable", this.getPackageName());
+                gifImageView.setImageResource(resourceGifId);
+                gifDrawable =(GifDrawable) gifImageView.getDrawable();
+//                gifDrawable.setLoopCount(2);
+//                imageView.setImageResource(resourceId);
                 textView.setText(scene.getString("text"));
+                int resourceAudioID = this.getResources().getIdentifier(scene.getString("audio"), "raw", this.getPackageName());
+                voice = MediaPlayer.create(this, resourceAudioID);
+                voice.start();
+                voice.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        gifDrawable.stop();
+                    }
+                });
             }
         }catch (JSONException e){
             e.printStackTrace();
@@ -129,6 +186,7 @@ public class SceneActivity extends AppCompatActivity {
             complete.put("marks", stelas);
             complete.put("score", score);
             complete.put("drawableCharac", drawableCharac);
+            complete.put("finish",false);
             fileWriter = new FileWriter(file.getAbsoluteFile());
             bufferedWriter = new BufferedWriter(fileWriter);
             bufferedWriter.write(complete.toString());
@@ -139,5 +197,18 @@ public class SceneActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(voice.isPlaying()){
+            voice.stop();
+        }
+        super.onBackPressed();
+    }
+
+    public void clearSharedPreferences(){
+        this.getSharedPreferences("imageSliderActivity", 0).edit().clear().apply();
+        this.getSharedPreferences("didYouKnowActicity", 0).edit().clear().apply();
     }
 }
